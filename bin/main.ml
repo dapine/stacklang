cab51feeaf7f4a2356bea9640ada4a3132d40238
David Pinheiro <davepinh@gmail.com>
2019-10-27 08:23:26 -0300

    Stack module renamed to Stacklang to avoid confusion with standard Stack module

--- bin/main.ml
+++ bin/main.ml
@@ -1,4 +1,4 @@
 let () =
     let lexbuf = Lexing.from_string "1 pop\n2 print\n3 push 10"
-    in let result = Stack.Parser.lines Stack.Lexer.token lexbuf
-    in List.iter (fun a -> print_endline (Stack.Syntax.string_of_line a)) result
+    in let result = Stacklang.Parser.lines Stacklang.Lexer.token lexbuf
+    in List.iter (fun a -> print_endline (Stacklang.Syntax.string_of_line a)) result
