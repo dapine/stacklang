238a410387a15878a64d612d33a2cdba6e2923f9
Breno Pinheiro <davepinh@gmail.com>
2019-11-04 15:38:48 -0300

    Basic command line interface added

--- bin/main.ml
+++ bin/main.ml
@@ -1,3 +1,14 @@
-Stacklang.Interpreter.from_file "example.sl"
+let usage = "Usage: stacklang help -> shows this message
+       stacklang repl -> starts a REPL session
+       stacklang [file] -> interprets the code from file"
 
-(* Stacklang.Interpreter.repl () *)
+let show_usage () =
+    print_endline usage
+
+let () =
+    if Array.length Sys.argv > 1
+    then match Sys.argv.(1) with
+            | "help" -> show_usage ()
+            | "repl" -> Stacklang.Interpreter.repl ()
+            | _ -> Stacklang.Interpreter.from_file Sys.argv.(1)
+    else show_usage ()
